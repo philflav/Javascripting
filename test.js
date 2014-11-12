9e50b2bfd5ad1fbbea199b4fc83715f61946e680
Phil <pflavin@gmail.com>
2014-11-12 19:47:09 +0000

    Express.js
    Express.js and test.js example from you Tube video

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-//test
+//get this at https://www.youtube.com/watch?v=QseHOX-5nJQ
+
+var express = require('express');
+
+var app=express();
+
+app.get('/', function (req, res){
+	res.send('Hello Express!');
+});
+
+app.listen(1337, function(){
+	console.log('Ready on port 1337');
+});
