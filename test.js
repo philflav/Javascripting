fecd006d43f5caf7055fa636a22412d6fbfcec21
Phil <pflavin@gmail.com>
2014-11-12 22:28:27 +0000

    ejs
    ejs installed and views set

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
 //get this at https://www.youtube.com/watch?v=QseHOX-5nJQ
 
 var express = require('express');
-
+var path = require('path');
 var app=express();
-
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
 app.get('/', function (req, res){
-	res.send('Hello Express!');
+	res.render('index');
 });
 
 app.listen(1337, function(){
